d762f98e381d7102e907b7d0b133caf914e5ad1c
Michael932322 <Michael.Alejandro.Lopez.valle@gmail.com>
2024-09-12 22:15:03 -0600

    Cambio 3

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -31,7 +31,7 @@ router.post('/login', async (req, res) => {
 });
 
 router.get('/protected-resource', (req, res) => {
-  const token = req.headers.authorization?.split(' ')[1];
+  const token = req.headers.authorization.split(' ')[1];
   if (!token) return res.status(401).json({ error: 'No token provided' });
 
   jwt.verify(token, SECRET_KEY, (err, decoded) => {
--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
 app.use('/api', authRoutes);
 
-mongoose.connect(process.env.MONGO_URI);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Failed to connect to MongoDB', err));
+
 
 app.listen(5000, () => {
   console.log('Server running on port 5000');
