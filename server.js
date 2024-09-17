6098e58be3330f8d04ab7c0d0aa3703c1008ebff
Astridlanda <landaastrid606@gmail.com>
2024-09-17 14:18:07 -0600

    Cambios 4

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-//Importaciones y configuración de dependencias
+//Importaciones y Configuración de Dependencias
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 require('dotenv').config();
 
-//Inicialización de la aplicación Express
+//Inicialización de la Aplicación Express
 const app = express();
 app.use(bodyParser.json());
 app.use('/api', authRoutes);
 
-//Conexión a MongoDB
+//Conexión a MongoDB 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Failed to connect to MongoDB', err));
 
-//Iniciar el servidor
+//Iniciar el Servidor 
 app.listen(5000, () => {
   console.log('Server running on port 5000');
 });
