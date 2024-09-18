d479a3b82361322719f7fe0a51ee8c999450b9da
WILLIAM IRAHETA <164599756+WillyUDB@users.noreply.github.com>
2024-09-17 19:18:05 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-//Importaciones y Configuración de Dependenciass
+//Importaciones y Configuración de Dependencias
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 require('dotenv').config();
 
-//Inicialización de la Aplicación Expresss
+//Inicialización de la Aplicación Express
 const app = express();
 app.use(bodyParser.json());
 app.use('/api', authRoutes);
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Failed to connect to MongoDB', err));
 
-//Iniciar el Servidorr 
+//Iniciar el Servidor
 app.listen(5000, () => {
   console.log('Server running on port 5000');
 });
